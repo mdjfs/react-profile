e57184665eab24c459d7dc9c3158119b398fd73a
u632763 <mdfuenmayorsoto@teco.com.ar>
2023-09-14 10:21:02 -0300

    fix imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-export { default as ReactProfile } from './ReactProfile';
+import ReactProfile from "./ReactProfile";
+
 export { openEditor } from './portal';
 export { ReactProfileProps, Change } from './types';
 export { ReactCropProps, type Crop } from 'react-image-crop'
 export { EXPORT_OBJECT, FILTERS_LIST as ALL_FILTERS } from 'react-pixels';
+
+export default ReactProfile
+export { ReactProfile }
